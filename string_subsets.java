c804a3d5c8813d5af38ee205b71d4e0339394ee1
47259_Shagun <chshagun72@gmail.com>
2021-10-04 20:54:44 +0530

    string subsets

--- string_subsets.java
+++ string_subsets.java
@@ -1,13 +1,28 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class string_subsets {
     static void findSubset(String str,String curr,int index){
-        if(index==str.length()){
-            System.out.println(curr);
-            return;
+        
+//with recursion		
+		//if(index==str.length()){
+        //    System.out.println(curr);
+        //   return;
+        //}
+        //findSubset(str,curr, index+1);
+        //findSubset(str, curr+str.charAt(index), index+1);
+		
+//Without Recursion
+		int len = str.length();  
+        int temp = 0;   
+        ArrayList<String> al = new ArrayList<>();  
+        for(int i=0; i<len; i++) {    
+            for(int j=i; j<len; j++) {  
+                al.add(str.substring(i, j+1));  
+               
+            }  
         }
-        findSubset(str,curr, index+1);
-        findSubset(str, curr+str.charAt(index), index+1);
+			System.out.println("Possible Subsets  : " + al);
     }
     public static void main(String[] args) {
         System.out.print("enter a String : ");
